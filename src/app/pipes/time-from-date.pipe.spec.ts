3eb2532da446806e3d21b1c455be85c508517309
maciejows <owsianny.maciej@gmail.com>
2021-02-19 12:00:01 +0100

    Added pipe's tests

--- src/app/pipes/time-from-date.pipe.spec.ts
+++ src/app/pipes/time-from-date.pipe.spec.ts
@@ -5,4 +5,26 @@ describe('TimeFromDatePipe', () => {
     const pipe = new TimeFromDatePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('Should get time', () => {
+    const input = "2021-02-19T09:20:17.429746Z";
+    const pipe = new TimeFromDatePipe();
+    const result = pipe.transform(input);
+    expect(result).toBe("09:20:17");
+  });
+
+  it('Should get time if "." not present', () => {
+    const input = "2021-02-19T09:20:17Z";
+    const pipe = new TimeFromDatePipe();
+    const result = pipe.transform(input);
+    expect(result).toBe("09:20:17");
+  });
+
+  it('Should get string till "." if "T" not present', () => {
+    const input = "2021-02-1909:20:17.429746Z";
+    const pipe = new TimeFromDatePipe();
+    const result = pipe.transform(input);
+    expect(result).toBe("2021-02-1909:20:17");
+  });
+
 });
